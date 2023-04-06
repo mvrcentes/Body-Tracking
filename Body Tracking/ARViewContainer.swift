661eae92b7f668130061a2db270defe63eb430cc
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-05 20:06:09 -0600

    finish the app

--- Body Tracking/ARViewContainer.swift
+++ Body Tracking/ARViewContainer.swift
@@ -5,4 +5,49 @@
 //  Created by Marco Ramirez on 5/04/23.
 //
 
-import Foundation
+import SwiftUI
+import ARKit
+import RealityKit
+
+private var bodySkeleton: BodySkeleton?
+private let bodySkeletonAnchor = AnchorEntity()
+
+
+struct ARViewContainer: UIViewRepresentable {
+    typealias UIViewType = ARView
+    
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        
+        arView.setupForBodyTracking()
+        arView.scene.addAnchor(bodySkeletonAnchor)
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
+
+extension ARView: ARSessionDelegate {
+    func setupForBodyTracking() {
+        let configuration = ARBodyTrackingConfiguration()
+        self.session.run(configuration)
+        
+        self.session.delegate = self
+    }
+    
+    public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let bodyAnchor = anchor as? ARBodyAnchor {
+                if let skeleton = bodySkeleton {
+                    skeleton.update(with: bodyAnchor)
+                } else {
+                    bodySkeleton = BodySkeleton(for: bodyAnchor)
+                    bodySkeletonAnchor.addChild(bodySkeleton!)
+                }
+            }
+        }
+    }
+}
