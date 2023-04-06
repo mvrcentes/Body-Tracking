3188a2b49d4803b4648ff8468ebe015f2d4cf348
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-05 20:05:54 -0600

    add SkeletonBone

--- Body Tracking/SkeletonBone.swift
+++ Body Tracking/SkeletonBone.swift
@@ -5,4 +5,17 @@
 //  Created by Marco Ramirez on 5/04/23.
 //
 
-import Foundation
+import RealityKit
+
+struct SkeletonBone {
+    var frameJoint: SkeletonJoint
+    var toJoint: SkeletonJoint
+    
+    var centerPosition: SIMD3<Float> {
+        [(frameJoint.position.x + toJoint.position.x)/2, (frameJoint.position.y + toJoint.position.y)/2, (frameJoint.position.z + toJoint.position.z)/2]
+    }
+    
+    var length: Float {
+        simd_distance(frameJoint.position, toJoint.position)
+    }
+}
