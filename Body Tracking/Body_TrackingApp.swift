32704e669ebf1d160f32aa99942f26154915799e
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-05 18:16:56 -0600

    Initial Commit

---
+++ Body Tracking/Body_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_TrackingApp.swift
+//  Body Tracking
+//
+//  Created by Marco Ramirez on 5/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Body_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
